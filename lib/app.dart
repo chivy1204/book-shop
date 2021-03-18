9d63c6e379636a9e364b7916ef200e3a5888292d
Nguyễn Chí Vỹ <steven@steven.local>
2021-03-18 14:00:19 +0700

    Create project

---
+++ lib/app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          print("Something went wrong!");
+          return Result();
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          print("My app");
+          return Result();
+        }
+        print("Loading");
+        return Result();
+      },
+    );
+  }
+}
+
+class Result extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("Hello");
+    return MaterialApp(
+        title: "Welcome to book shop",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Book Shop"),
+          ),
+          body: Center(
+            child: Text("List Book"),
+          ),
+        ));
+  }
+}
+
+class Load extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print("Hello");
+    return MaterialApp(
+        title: "Welcome to book shop",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Book Shop"),
+          ),
+          body: Center(
+            child: Text("Loading"),
+          ),
+        ));
+  }
+}
